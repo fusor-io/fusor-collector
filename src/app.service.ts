628c528d0c11fc51b5ea0c44d64cb280eaf7950e
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-03-07 13:24:07 +0200

    fix(reload): start LogLevelManager after jobs relaod

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+import { LogLevelManagerService } from './services/log-level-manager/service/log-level-manager.service';
 import { SchedulerService } from './services/scheduler/service/scheduler.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly _schedulerService: SchedulerService) {}
+  constructor(
+    private readonly _schedulerService: SchedulerService,
+    private readonly _logLevelManager: LogLevelManagerService,
+  ) {}
 
   healthCheck(): Record<string, string> {
     return { status: 'ok' };
   }
 
   async reload(): Promise<void> {
-    return this._schedulerService.reload();
+    await this._schedulerService.reload();
+    this._logLevelManager.scheduleLevelUpdate;
   }
 }
